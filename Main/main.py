ec60a5381f30844ec581dd9f320f42b406f87d53
chad <cdrap27@wgu.edu>
2023-02-16 09:16:51 -0500

    more bones

--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,8 @@
+import Model.distance;
+
+miles = [1, 2, 3];
+
+da = Model.distance.Distance("WGU", "5198 east side street", miles);
+
 print("hello world");
+print(da.miles);
---
+++ Model/distance.py
@@ -0,0 +1,5 @@
+class Distance():
+    def __init__(self, name, address, miles):
+        self.name = name;
+        self.address = address;
+        self.miles = miles;
