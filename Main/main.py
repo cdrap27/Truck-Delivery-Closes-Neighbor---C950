60492a5166746b2a04952f07bc7efc9c1782e7cf
chad <cdrap27@wgu.edu>
2023-02-24 08:49:58 -0500

    hashMap

--- Main/main.py
+++ Main/main.py
@@ -2,9 +2,21 @@ import Model.distance;
 import CSVRead.hubDistance
 import CSVRead.packages
 import Model.package
+import hashMap.packageHash
 
 CSVRead.hubDistance.createDisList()
 pack = CSVRead.packages.setPackageList().copy()
 
 print(CSVRead.hubDistance.disList[1].name)
 print(CSVRead.packages.packageList[2].specialNotes)
+
+h = hashMap.packageHash.packageHash()
+
+for item in CSVRead.packages.packageList:
+    h.add(item.pack, item)
+
+h.print()
+
+yo =h.get('3')
+
+print(yo.specialNotes)
