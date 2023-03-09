e1fcb412120dae5513385adf68d055d5de4ac87b
chad <cdrap27@wgu.edu>
2023-03-09 07:56:03 -0500

    package list has comments

--- CSVRead/packages.py
+++ CSVRead/packages.py
@@ -1,8 +1,20 @@
 import csv
 import Model.package
-
+"""
+A variable is created as a list.
+"""
 packageList = []
+"""
+A function is created that creates a list of packages
+"""
 def setPackageList():
+    """
+    The function opens the list of packages as a CSV file and separates each value with a comma.
+    The function then separates each line and row using 2 for loops.  Each line is stored using the package class.
+    The package is then added the the packageList.
+    The function has nested for loops and has a time complexity of O(n^2)
+    :return: returns a list of packages.
+    """
     with open('packages.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
         for row in csv_reader:
