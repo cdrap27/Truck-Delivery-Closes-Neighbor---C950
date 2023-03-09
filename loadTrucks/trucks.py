b1fb028dc1a0fdf1d0fdbba99774f5a74367b725
chad <cdrap27@wgu.edu>
2023-03-09 09:10:25 -0500

    load trucks comments

--- loadTrucks/trucks.py
+++ loadTrucks/trucks.py
@@ -1,16 +1,30 @@
 import Model.truck
-
+"""
+creates 2 variables of type list
+"""
 pack1 = []
 pack2 = []
-
+"""
+creates 2 trucks
+"""
 truck1 = Model.truck.Truck(pack1)
 truck2 = Model.truck.Truck(pack2)
+"""
+list of packages for each run, manually loaded
+"""
 t1run1 = ['1', '2', '4', '5', '7', '12', '27', '29', '30', '33', '35', '39', '40']
 t2run1 = ['13', '14', '15', '16', '18', '19', '20', '21', '22', '24', '31',  '34', '36', '37', '38']
 t1run2 = ['6',  '25']
 t2run2 = ['3',  '8', '9', '10', '11', '17',  '23', '26', '28', '32']
 
 def load_truck(h, truck, run):
+    """
+    clears the trucks package list and adds packages to the truck using a for loop O(n)
+    :param h: hash map of packages
+    :param truck: truck to be loaded
+    :param run: list of packages on each run
+    :return: returns truck loaded with packages
+    """
     truck.packages.clear()
     for item in run:
         truck.packages.append(h.get(item))
